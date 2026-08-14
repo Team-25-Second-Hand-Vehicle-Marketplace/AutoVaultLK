68353ade729ebea25b02f405740904980bf0cc15
rash200319 <pabodarashmi668@gmail.com>
2026-08-14 23:14:29 +0530

    feat(auth): add internal user deactivate for admin
    East-west POST /internal/users/:id/deactivate wraps adminUpdate and rejects self-deactivation.

--- auth-user-service/src/modules/users/users.module.ts
+++ auth-user-service/src/modules/users/users.module.ts
@@ -2,14 +2,16 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../infrastructure/database/entities/user.entity';
 import { JwtAuthModule } from '../auth/jwt-auth.module';
+import { InternalUsersController } from './controllers/internal-users.controller';
 import { UsersController } from './controllers/users.controller';
 import { UsersRepository } from './repositories/users.repository';
 import { UsersService } from './services/users.service';
+import { InternalServiceGuard } from '../../common/guards/internal-service.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), forwardRef(() => JwtAuthModule)],
-  controllers: [UsersController],
-  providers: [UsersRepository, UsersService],
+  controllers: [UsersController, InternalUsersController],
+  providers: [UsersRepository, UsersService, InternalServiceGuard],
   exports: [UsersRepository, UsersService],
 })
 export class UsersModule {}
