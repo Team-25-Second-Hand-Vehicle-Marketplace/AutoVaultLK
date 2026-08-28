beae6173a09e709ef6e64320d9d1f95befefacc1
Mystery Box <janithv.23@cse.mrt.ac.lk>
2026-08-28 13:42:53 +0530

    CSS Files changed

--- web-frontend/src/main.tsx
+++ web-frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import './index.css'
 import './styles/app.css'
 import './styles/theme.css'
 import './styles/home.css'
