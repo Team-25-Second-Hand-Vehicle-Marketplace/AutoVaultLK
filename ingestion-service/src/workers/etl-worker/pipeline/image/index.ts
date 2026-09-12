9ff154fe0fae439521fe3b7b5766f44b839ff62f
virusan-T <thavamviru1110@gmail.com>
2026-09-12 17:12:40 +0530

    feat:zip extraction portion

---
+++ ingestion-service/src/workers/etl-worker/pipeline/image/index.ts
@@ -0,0 +1,9 @@
+export {
+  extractImagesStage,
+} from './extract-images.stage';
+
+export type {
+  ExtractImagesInput,
+  ExtractImagesOutput,
+  ExtractedImage,
+} from './extract-images.stage';
